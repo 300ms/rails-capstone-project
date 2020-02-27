44e6534c47a63767fa4b156ec20efe88919ff28c
300ms <ilhansnmz44@gmail.com>
2020-02-27 18:34:33 +0300

    Categories have been added into seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-5.times do |n|
-  name  = Faker::Name.name
-  Category.create!(name: name, priority: 0)
-end
+Category.create!(name: "Java", priority: 1)
+Category.create!(name: "Ruby", priority: 2)
+Category.create!(name: "C/C++", priority: 3)
+Category.create!(name: "Python", priority: 4)
+Category.create!(name: "Assembly", priority: 5)
