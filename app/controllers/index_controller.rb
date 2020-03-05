29d2230b4bc2576a42df5e78075004b48b61c541
300ms <ilhansnmz44@gmail.com>
2020-03-05 21:18:59 +0300

    Working on requested changes

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,12 +3,17 @@
 class IndexController < ApplicationController
   def index
     @articles = Article.all.to_a
-    @top5 = {}
+    @top1 = {}
     @articles.each do |article|
-      @top5[article.id] = article.votes.count
+      @top1[article.id] = article.votes.count
     end
-    @top5 = @top5.sort_by { |_id, votes| votes }
-    @top5 = @top5.last(3)
-    @top5 = Hash[@top5.to_a.reverse]
+    @top1 = @top1.sort_by { |_id, votes| votes }
+    @top1 = @top1.last(1)
+
+    @cat_1 = Article.find_by(category_id: 1)
+    @cat_2 = Article.find_by(category_id: 2)
+    @cat_3 = Article.find_by(category_id: 3)
+    @cat_4 = Article.find_by(category_id: 4)
+    @cat_5 = Article.find_by(category_id: 5)
   end
 end
