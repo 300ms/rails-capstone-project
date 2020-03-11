db868bb46686f38f64541689c40c490473df5481
300ms <ilhansnmz44@gmail.com>
2020-03-11 12:40:13 +0300

    Sorting/ordering has been removed from controller and added into model

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,14 +2,9 @@
 
 class IndexController < ApplicationController
   def index
-    @articles = Article.all.to_a
-    @top1 = {}
-    @articles.each do |article|
-      @top1[article.id] = article.votes.count
-    end
-    @top1 = @top1.sort_by { |_id, votes| votes }
-    @top1 = @top1.last(1)
 
+
+    @top1 = Article.find(Article.top_voted(1)[0].id)
     @cat1 = Article.find_by(category_id: 1)
     @cat2 = Article.find_by(category_id: 2)
     @cat3 = Article.find_by(category_id: 3)
--- app/models/article.rb
+++ app/models/article.rb
@@ -7,4 +7,12 @@ class Article < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :text, presence: true, length: { maximum: 140 }
   mount_uploader :image, ImageUploader
+
+  def self.top_voted(num = 10)
+    select("articles.id, count(votes.id) AS votes_count")
+    .joins(:votes)
+    .group("articles.id")
+    .order("votes_count DESC")
+    .limit(num)
+  end
 end
